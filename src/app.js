bb080b92c1bdda352ded3008ed010892d70a67df
BeneToko <131210264+BeneToko@users.noreply.github.com>
2023-04-26 22:17:12 +0100

    humidity and wind elements added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  let cityElement = documnet.querySelector("#city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement = response.data.condition.description;
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#speed");
+  windElement = response.data.wind.speed;
 }
-
 let apiKey = "tfa2776a4d92b34b1190063a36d411of";
 let city = "New York";
 let units = "metric";
