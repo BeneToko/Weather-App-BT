f765257ac14ebbc81da993937dfafce5fa45c06f
BeneToko <131210264+BeneToko@users.noreply.github.com>
2023-04-26 21:43:43 +0100

    added js api elements

---
+++ src/app.js
@@ -0,0 +1,14 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = documnet.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement = response.data.condition.description;
+}
+
+let apiKey = "tfa2776a4d92b34b1190063a36d411of";
+let city = "New York";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(displayTemperature);
